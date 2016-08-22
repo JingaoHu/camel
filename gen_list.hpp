1865d9f656856629f8e8d134b9d22451e382ba9c
JingaoHu <hujingao2014@ia.ac.cn>
2016-08-22 20:37:06 +0800

    generate a unsorted list

---
+++ gen_list.hpp
@@ -0,0 +1,15 @@
+#ifndef GEN_LIST
+#define GEN_LIST
+
+#include<stdlib.h>
+void gen_list(int a[],int len,int min,int max)
+{
+	int i;
+	for(i=0;i<len;i++)
+	{
+		a[i]=(rand()%(max-min))+min;
+	}
+	return 0;
+}
+
+#endif
