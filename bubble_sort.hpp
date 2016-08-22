4b450336b5de466784af4ea95624281ff47e14b5
JingaoHu <hujingao2014@ia.ac.cn>
2016-08-22 22:59:01 +0800

    fix bugs

--- bubble_sort.hpp
+++ bubble_sort.hpp
@@ -10,8 +10,8 @@ void swap(int &a,int &b)
 void bubble_sort(int a[],int len)
 {
 	int i,j;
-	for(i=0;i<len;i++)
-		for(j=0;j<len-i;j++)
+	for(i=0;i<len-1;i++)
+		for(j=0;j<len-1-i;j++)
 			if(a[j]>a[j+1])
 				swap(a[j],a[j+1]);
 	return ;	
