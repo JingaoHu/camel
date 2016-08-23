d0f6aec8226b5a930aae886a7058a24070da682a
JingaoHu <hujingao2014@ia.ac.cn>
2016-08-23 07:28:47 +0000

    chs

--- array/gen_array.hpp
+++ array/gen_array.hpp
@@ -1,8 +1,16 @@
+
+//------------------------------------------------------------
+//   function: generate an unsorted int array
+//   written by Jingao Hu
+//------------------------------------------------------------ 
+
+
+
 #ifndef GEN_LIST
 #define GEN_LIST
 
 #include<stdlib.h>
-void gen_list(int a[],int len,int min,int max)
+void gen_array(int a[],int len,int min,int max)
 {
 	int i;
 	for(i=0;i<len;i++)
