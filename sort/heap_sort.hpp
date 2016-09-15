405d46f9a5d3cbb2eca219cc2034abea2b3b1c28
JingaoHu <hujingao2014@ia.ac.cn>
2016-09-15 20:16:03 +0800

    heap-sort

---
+++ sort/heap_sort.hpp
@@ -0,0 +1,48 @@
+#define parent(i) ((i-1)>>2)
+#define left(i) ((i<<1)+1)
+#define right(i) ((i<<1)+2)
+
+inline void swap(int &a, int &b)
+{
+	int temp=a;
+	a=b;
+	b=temp;
+	return ;
+}
+
+void max_heapify(int a[],int len,int root)
+{
+	if(root>=len)
+		return ;
+	int max;
+	if(left(root)<len && right(root)<len)
+		max=a[left(root)]>a[right(root)]?left(root):right(root);
+	else if(left(root)<len && right(root)>=len)
+			max=left(root);
+		else
+			max=root;
+		
+	if(a[root]<a[max])
+	{
+		swap(a[root],a[max]);
+		max_heapify(a,len,max);	
+	}
+	return ;
+}
+void build_max_heap(int a[],int len)
+{
+	for(int i=len-1;i>=0;i--)
+		max_heapify(a,len,i);
+	return ;
+}
+
+void heap_sort(int a[],int len)
+{
+	build_max_heap(a,len);
+	for(int i=0;i<len;i++)
+	{
+		swap(a[0],a[len-1-i]);
+		max_heapify(a,len-i-1,0);
+	}
+	return ;
+}
